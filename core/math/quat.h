ad7b5a891caa7dd0cddd64674fa5e6263a318c55
Marcel Admiraal <madmiraal@users.noreply.github.com>
2020-09-04 09:18:19 +0100

    Ensure assignment operators return by reference to avoid unnecessary copies.

--- core/math/quat.h
+++ core/math/quat.h
@@ -117,9 +117,13 @@ public:
 			z(p_z),
 			w(p_w) {
 	}
-	Quat(const Vector3 &axis, const real_t &angle) { set_axis_angle(axis, angle); }
+	Quat(const Vector3 &axis, const real_t &angle) {
+		set_axis_angle(axis, angle);
+	}
 
-	Quat(const Vector3 &euler) { set_euler(euler); }
+	Quat(const Vector3 &euler) {
+		set_euler(euler);
+	}
 	Quat(const Quat &p_q) :
 			x(p_q.x),
 			y(p_q.y),
@@ -127,7 +131,7 @@ public:
 			w(p_q.w) {
 	}
 
-	Quat operator=(const Quat &p_q) {
+	Quat &operator=(const Quat &p_q) {
 		x = p_q.x;
 		y = p_q.y;
 		z = p_q.z;
